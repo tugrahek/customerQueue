b52c1c73785fd2a186f07cba6c6decb8cb2260a7
Tugra Hekimoglu <162835393+tugrahek@users.noreply.github.com>
2026-04-23 21:16:45 +0300

    added the customerQueue class

--- src/customerqueue/CustomerQueue.java
+++ src/customerqueue/CustomerQueue.java
@@ -1,13 +1,44 @@
 
 package customerqueue;
 
+import java.util.LinkedList;
+
 
 public class CustomerQueue {
+    LinkedList<Customer> customerQueue;
 
-    
-    public static void main(String[] args) {
-        
-        
+    public CustomerQueue() {
+        this.customerQueue = new LinkedList<>();
     }
+    
+    public void addCustomer(Customer customer){
+        customerQueue.add(customer);
+    }
+    public Customer removeCustomer(){
+        if(customerQueue.isEmpty()){
+            return null;
+        }
+        return customerQueue.remove();
+    }
+    public Customer peekCustomer(){
+        if(customerQueue.isEmpty()){
+            return null;
+        }
+         return customerQueue.peek();
+    }
+    public void printElements(){
+        for(Customer customer: customerQueue){
+            System.out.println(customer);
+        }
+    }
+    public int size(){
+        return customerQueue.size();
+    }
+    
+    public LinkedList<Customer> getQueueLinkedList(){
+        return customerQueue;
+    }
+    
+    
     
 }
---
+++ src/customerqueue/Demo.java
@@ -0,0 +1,13 @@
+
+package customerqueue;
+
+
+public class Demo {
+
+    
+    public static void main(String[] args) {
+        
+        
+    }
+    
+}
